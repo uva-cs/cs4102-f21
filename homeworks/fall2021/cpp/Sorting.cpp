e9f3c7c8b9ef90d0655fd52f3e924155e8f49a23
Mark Floryan <floryan@floryan.local>
2021-08-25 15:36:15 -0400

    adding code for new sorting hw

---
+++ homeworks/fall2021/cpp/Sorting.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void insertionSort(vector<int> &list) {
+	//TODO: WRITE THIS METHOD
+}
+
+
+
+int partition(vector<int> &list, int i, int j) {
+	//TODO: PROBABLY WANT THIS FOR QUICKSORT
+}
+
+void quickSort(vector<int> &list, int i, int j) {
+	//TODO: WRITE THIS METHOD
+}
+
+void quickSort(vector<int> &list) {
+	//TODO: DRIVER CALLS THIS METHOD, MIGHT WANT TO JUST CALL THE OTHER HELPER ONE ABOVE
+}
+
+
+
+
---
+++ homeworks/fall2021/java/Sorting.java
@@ -0,0 +1,26 @@
+
+import java.util.HashMap;
+
+public class Sorting {
+
+	public static<T extends Comparable<T>> void insertionSort(T[] list) {
+		//TODO: IMPLEMENT THIS METHOD
+	}
+	
+	
+
+	
+	
+	public static<T extends Comparable<T>> void quickSort(T[] list) {
+		//TODO: IMPLEMENT THIS METHOD
+	}
+	
+	private static<T extends Comparable<T>> void quickSort(T[] list, int i, int j) {
+		//TODO HELPER METHOD FOR QUICKSORT THAT TAKES IN INDICES
+	}
+	
+	private static<T extends Comparable<T>> int partition(T[] list, int i, int j) {
+		//TODO: PARTITION THE PORTION OF THE LIST BETWEEN i AND j
+	}
+
+}
