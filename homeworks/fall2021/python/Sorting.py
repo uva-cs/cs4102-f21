6e1256f3243f79b666c5a5cf957db0e41fd3dfd6
Mark Floryan <floryan@d-172-27-138-213.dhcp.virginia.edu>
2021-08-27 10:06:00 -0400

    adding python code for sorting hw

---
+++ homeworks/fall2021/python/Sorting.py
@@ -0,0 +1,13 @@
+
+def insertionSort(list):
+	#TODO: IMPLEMENT THIS METHOD
+
+def partition(list, i, j):
+	#TODO: PARTITION FOR QUICKSORT
+
+def quickSortHelper(list, i, j):
+	#TODO: VERSION OF QUICKSORT THAT ACCEPTS ARRAY INDICES
+
+
+def quickSort(list):
+	#TODO: IMPLEMENT QUICKSORT (THIS ONE IS THE ONE CALLED BY THE DRIVER) 
